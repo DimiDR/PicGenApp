e2f24faa6cf2fbaac2292db671cdc90b9fc018fe
Dimitri Rybak <dima_rybak@yahoo.de>
2023-06-08 16:08:25 +0200

    changes

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, Image, TextInput, Button, Text } from "react-native";
 import Checkbox from "expo-checkbox";
+import requests from "requests";
 import React, { useState } from "react";
 import axios from "axios"; // For making API requests
 
@@ -8,28 +9,62 @@ export default function App() {
   const [generatedImage, setGeneratedImage] = useState(
     "https://picsum.photos/200/300"
   );
-  const [positivePrompt, setPositivePrompt] = useState("");
+  const [positivePrompt, setPositivePrompt] = useState("Big");
   const [negativePrompt, setNegativePrompt] = useState("");
   const [allowAdultContent, setAllowAdultContent] = useState(false);
+  const [image, setImage] = useState();
+  const [prompt, setPrompt] = useState();
+  const [loading, setLoading] = useState(false);
 
   const generatePicture = async () => {
+    setLoading(true);
+    setPrompt("Dimitri");
     try {
+      payload = {
+        prompt: "maltese puppy",
+        steps: 5,
+      };
       // Send the positive, negative prompts, and adult content flag to the backend server
-      const response = await axios.post("YOUR_BACKEND_API_URL", {
-        positivePrompt,
-        negativePrompt,
-        allowAdultContent,
-      });
+      const response = await axios.post(
+        `http://127.0.0.1:7861/sdapi/v1/txt2img?prompt=${positivePrompt}`
+        // {
+        //   positivePrompt,
+        //   negativePrompt,
+        //   allowAdultContent,
+        // }
+      );
 
       // Get the generated picture URL from the server response
       const { generatedPicture } = response.data;
 
-      // Update the state with the generated picture
+      // set the state with the generated picture
       setGeneratedImage(generatedPicture);
+      setLoading(false);
     } catch (error) {
       console.error("Error generating picture:", error);
+      setLoading(false);
     }
   };
+
+  // const generatePicture = async () => {
+  //   const url = "http://127.0.0.1:7861";
+
+  //   const payload = {
+  //     prompt: "puppy dog",
+  //     steps: 5,
+  //   };
+
+  //   const response = await fetch(`${url}/sdapi/v1/txt2img`, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(payload),
+  //   });
+
+  //   const r = await response.json();
+  //   alert(r);
+  // };
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # PicGenApp
+
 App to generate Pictures with AI
 
 React Native App with:
+
 - One area for picture display
 - One area for positiv prompt
 - One are for negativ prompt
@@ -9,6 +11,7 @@ React Native App with:
 - One checkmark "allow adult content"
 
 # Links
+
 Stable Diffusion as an API: Make a Person-Removing Microservice
 https://towardsdatascience.com/stable-diffusion-as-an-api-5e381aec1f6
 
@@ -26,3 +29,75 @@ https://stablediffusionapi.com/docs/
 
 API Docs Automatic1111
 https://github.com/AUTOMATIC1111/stable-diffusion-webui/wiki/API
+
+Basic API Doku
+https://github.com/AUTOMATIC1111/stable-diffusion-webui/discussions/3734
+
+http://127.0.0.1:7860/docs#/
+
+# launch backend server
+
+C:\Users\Rybak\Documents\StableDiffusion>python launch.py --nowebui --cors-allow-origins "\*
+
+## CORS Problem
+
+https://github.com/AUTOMATIC1111/stable-diffusion-webui/discussions/3734#discussioncomment-4786786
+You're using it wrong.
+--cors-allow-origins "localhost"
+If that doesn't work, add the port of your frontend.
+You can add multiple, separated by commas if i'm not mistaken.
+I used this API in 100 requests/hour+ production app in september so I know for sure that it's atleast been working.
+
+You can also use --cors-allow-origins "\*" locally. But in production, use your frontend url.
+
+# sdapi/v1/txt2img
+
+{
+"enable_hr": false,
+"denoising_strength": 0,
+"firstphase_width": 0,
+"firstphase_height": 0,
+"hr_scale": 2,
+"hr_upscaler": "string",
+"hr_second_pass_steps": 0,
+"hr_resize_x": 0,
+"hr_resize_y": 0,
+"hr_sampler_name": "string",
+"hr_prompt": "",
+"hr_negative_prompt": "",
+"prompt": "",
+"styles": [
+"string"
+],
+"seed": -1,
+"subseed": -1,
+"subseed_strength": 0,
+"seed_resize_from_h": -1,
+"seed_resize_from_w": -1,
+"sampler_name": "string",
+"batch_size": 1,
+"n_iter": 1,
+"steps": 50,
+"cfg_scale": 7,
+"width": 512,
+"height": 512,
+"restore_faces": false,
+"tiling": false,
+"do_not_save_samples": false,
+"do_not_save_grid": false,
+"negative_prompt": "string",
+"eta": 0,
+"s_min_uncond": 0,
+"s_churn": 0,
+"s_tmax": 0,
+"s_tmin": 0,
+"s_noise": 1,
+"override_settings": {},
+"override_settings_restore_afterwards": true,
+"script_args": [],
+"sampler_index": "Euler",
+"script_name": "string",
+"send_images": true,
+"save_images": false,
+"alwayson_scripts": {}
+}
